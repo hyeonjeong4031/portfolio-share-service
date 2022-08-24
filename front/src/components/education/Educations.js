9002bb381184353fb753784d90e45db729c88491
Lizzie <leejihye7117@gmail.com>
2022-08-24 13:42:18 +0900

    educations.js

--- front/src/components/education/Educations.js
+++ front/src/components/education/Educations.js
@@ -1,19 +1,46 @@
-import React from "react";
-import { Card, Button, Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Card, Button, Col, Row } from "react-bootstrap";
+import * as Api from "../../api";
+import Education from "./Education";
+import EducationAddForm from "./EducationAddForm";
 
-const Educations = () => {
+const Educations = ({ portfolioOwnerId, isEditable }) => {
+  const [educations, setEducations] = useState([]);
+  const [isAdding, setIsAdding] = useState(false);
+
+  useEffect(() => {
+    Api.get("educationlist", portfolioOwnerId).then((res) =>
+      setEducations(res.data)
+    );
+  }, [portfolioOwnerId]);
+
   return (
     <Card>
       <Card.Body>
         <Card.Title>학력</Card.Title>
+        {educations.map((education) => (
+          <Education
+            key={education.id}
+            education={education}
+            setEducations={setEducations}
+            isEditable={isEditable}
+          />
+        ))}
+        {isEditable && (
+          <Row className="mt-3 text-center mb-4">
+            <Col sm={{ span: 20 }}>
+              <Button onClick={() => setIsAdding(true)}>+</Button>
+            </Col>
+          </Row>
+        )}
+        {isAdding && (
+          <EducationAddForm
+            portfolioOwnerId={portfolioOwnerId}
+            setEducations={setEducations}
+            setIsAdding={setIsAdding}
+          />
+        )}
       </Card.Body>
-      <Row>
-        <Col className="text-center mb-3">
-          <Button variant="primary" className="sm">
-            +
-          </Button>
-        </Col>
-      </Row>
     </Card>
   );
 };
