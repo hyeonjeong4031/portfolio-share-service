8b7f906e057d951f7fec8c7a014e1692c2db6d88
Lizzie <leejihye7117@gmail.com>
2022-08-24 13:42:56 +0900

    EducationEditForm.js

--- front/src/components/education/EducationEditForm.js
+++ front/src/components/education/EducationEditForm.js
@@ -1,5 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import * as Api from "../../api";
 
-const EductaionEditForm = () => {};
+const EducationEditForm = ({
+  currentEducation,
+  setEducations,
+  setIsEditing,
+}) => {
+  const [school, setSchool] = useState(currentEducation.school);
+  const [major, setMajor] = useState(currentEducation.major);
+  const [position, setPosition] = useState(currentEducation.position);
 
-export default EductaionEditForm;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user_id = currentEducation.user_id;
+
+    await Api.put(`educations/${currentEducation.id}`, {
+      user_id,
+      school,
+      major,
+      position,
+    });
+
+    const res = await Api.get("educationlist", user_id);
+    setEducations(res.data);
+    setIsEditing(false);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="educationEditSchool">
+        <Form.Control
+          type="text"
+          placeholder="학교 이름"
+          value={school}
+          onChange={(e) => setSchool(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="educationEditMajor" className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="전공"
+          value={major}
+          onChange={(e) => setMajor(e.target.value)}
+        />
+      </Form.Group>
+
+      <div key={`inline-radio`} className="mb-3 mt-3">
+        <Form.Check
+          inline
+          label="재학중"
+          id="radio-edit-1"
+          type="radio"
+          name="position"
+          value="재학중"
+          checked={position === "재학중"}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+        <Form.Check
+          inline
+          label="학사졸업"
+          id="radio-edit-2"
+          type="radio"
+          name="position"
+          value="학사졸업"
+          checked={position === "학사졸업"}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+        <Form.Check
+          inline
+          label="석사졸업"
+          id="radio-edit-3"
+          type="radio"
+          name="position"
+          value="석사졸업"
+          checked={position === "석사졸업"}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+        <Form.Check
+          inline
+          label="박사졸업"
+          id="radio-edit-4"
+          type="radio"
+          name="position"
+          value="박사졸업"
+          checked={position === "박사졸업"}
+          onChange={(e) => setPosition(e.target.value)}
+        />
+      </div>
+
+      <Form.Group as={Row} className="mt-3 text-center mb-4">
+        <Col sm={{ span: 20 }}>
+          <Button variant="primary" type="submit" className="me-3">
+            확인
+          </Button>
+          <Button variant="secondary" onClick={() => setIsEditing(false)}>
+            취소
+          </Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  );
+};
+
+export default EducationEditForm;
