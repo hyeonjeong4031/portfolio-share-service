42505cf0a2858fe846bfd59ac44e8d3789230202
Lizzie <leejihye7117@gmail.com>
2022-08-24 13:42:27 +0900

    education.js

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -1,6 +1,26 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+import React, { useState } from "react";
+import EducationCard from "./EducationCard";
+import EducationEditForm from "./EducationEditForm";
 
-const Education = () => {};
+const Education = ({ education, setEducations, isEditable }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <>
+      {isEditing ? (
+        <EducationEditForm
+          currentEducation={education}
+          setEducations={setEducations}
+          setIsEditing={setIsEditing}
+        />
+      ) : (
+        <EducationCard
+          education={education}
+          isEditable={isEditable}
+          setIsEditing={setIsEditing}
+        />
+      )}
+    </>
+  );
+};
 
 export default Education;
