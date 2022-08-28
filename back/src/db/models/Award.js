47859d840484837ee1c3b4aa6dad42eacea20553
ChyoPyoRo <allen15590@naver.com>
2022-08-28 16:32:38 +0900

    수정, 다른 유저 정보불러오기

--- back/src/db/models/Award.js
+++ back/src/db/models/Award.js
@@ -6,16 +6,26 @@ class Award {
     return createdNewAward;
   }
 
-  static async findById({ user_id ,title }) {
-    const award_this = await AwardModel.findOne({ id: user_id , title : title});
-    return award_this;
-  }
+  
 
   static async findAll(data) {
     // console.log('this is it',data.writer);
     const award_all = await AwardModel.find({writer_id : data.writer});
     // const award_all = await AwardModel.find({});
     return award_all;
+  }
+
+  static async findByAwardId({awardID}){
+    const award_this = await AwardModel.findOne({id: awardID});
+    return award_this;
+  }
+
+  static async fixOneAward({filter, data}){
+    console.log(filter)
+    console.log(data)
+    const fixedAward = await AwardModel.updateOne({id : filter}, data)
+    console.log(fixedAward)
+    return fixedAward
   }
 }
 
